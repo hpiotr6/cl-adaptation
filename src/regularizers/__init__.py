de8133368f59d913fa5608893dda6b44ac27f0ee
Piotr Hondra <piotr.hondra@gmail.com>
2024-02-19 17:28:11 +0100

    feat: Add deeper reg possibility

--- src/regularizers/__init__.py
+++ src/regularizers/__init__.py
@@ -9,7 +9,7 @@ import torch
 class VarCovRegLossProtocol(Protocol):
     def __call__(
         self, model: torch.nn.Module, inputs: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.LongTensor]: ...
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: ...
 
 
 @dataclass
@@ -22,40 +22,41 @@ class VarCovRegLoss(VarCovRegLossProtocol):
     hooks: defaultdict = field(default_factory=lambda: defaultdict(lambda: None))
     scale_strategy: Callable[[torch.Tensor], torch.Tensor] = lambda feats: feats
 
-    # def initialise_hooks(self, model):
-    #     def hook_fn(name):
-    #         def hook(module, input, output):
-    #             self.hooks[name] = output
+    def initialise_hooks(self, model):
+        def hook_fn(name):
+            def hook(module, input, output):
+                self.hooks[name] = output
 
-    #         return hook
+            return hook
 
-    #     # Initialize hooks on specified layers
-    #     for layer_name in self.layer_names_to_hook:
-    #         layer = dict(model.named_children())[layer_name]
-    #         self.hooks[layer_name] = layer.register_forward_hook(hook_fn(layer_name))
+        # Initialize hooks on specified layers
+        for layer_name in self.layer_names_to_hook:
+            layer = dict(model.named_children())[layer_name]
+            self.hooks[layer_name] = layer.register_forward_hook(hook_fn(layer_name))
 
     def __call__(self, model: torch.nn.Module, inputs: torch.Tensor):
+        if self.layer_names_to_hook is None:
+            last_layer_name = list(model.named_modules())[-1][0]
+            self.layer_names_to_hook = [last_layer_name]
+
+        if not self.initialised:
+            self.initialise_hooks(model)
+
         feats = model(inputs)
-        v, c = self.regularize_step(feats)
-        # feats = feats - feats.mean(dim=0)
+
+        variance_sum = 0
+        covariance_sum = 0
+
+        for hook in [*self.hooks.values()]:
+            v, c = self.regularize_step(hook)
+            variance_sum += v
+            covariance_sum += c
+
         return (
-            v * self.vcr_var_weight,
-            c * self.vcr_cov_weight,
+            self.vcr_var_weight * variance_sum,
+            self.vcr_cov_weight * covariance_sum,
             self.scale_strategy(feats),
         )
-        # variance_sum = 0
-        # covariance_sum = 0
-
-        # for hook in [*self.hooks.values()]:
-        #     v, c = self.regularize_step(hook)
-        #     variance_sum += v
-        #     covariance_sum += c
-
-        # return (
-        #     self.vcr_var_weight * variance_sum,
-        #     self.vcr_cov_weight * covariance_sum,
-        #     feats,
-        # )
 
     def regularize_step(self, feats):
         flattened_input = feats.flatten(start_dim=0, end_dim=-2)
@@ -75,6 +76,5 @@ class NullVarCovRegLoss(VarCovRegLossProtocol):
 
     def __call__(self, model, inputs):
         feats = model(inputs)
-        # feats = feats - feats.mean(dim=0)
 
         return self.dummy_zero, self.dummy_zero, self.scale_strategy(feats)
